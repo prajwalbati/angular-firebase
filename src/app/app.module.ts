964331665e07f6cfdb71fe701488151e3307c069
Prajwal Bati <prajwal.bati@ekbana.info>
2019-09-23 17:03:31 +0545

    ORder form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { OrdersService } from "./shared/orders.service";
+
 import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderListComponent } from './order-list/order-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    OrderListComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [OrdersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/orders/orders.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
+
+import { OrdersService } from "../shared/orders.service";
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.scss']
+})
+export class OrdersComponent implements OnInit {
+
+  constructor(
+    private ordersService: OrdersService
+  ) { }
+
+  form = new FormGroup({
+    customerName: new FormControl(''),
+    orderNumber: new FormControl(''),
+    coffeeOrder: new FormControl(''),
+    completed: new FormControl(false)
+  });
+  coffees = ["Americano", "Flat White", "Cappuccino", "Latte", "Espresso", "Machiato", "Mocha", "Hot Chocolate", "Tea"];
+
+  coffeeOrder = [];
+
+  ngOnInit() {
+  }
+
+  addCoffee = coffee => this.coffeeOrder.push(coffee);
+
+  removeCoffee = coffee => {
+      let index = this.coffeeOrder.indexOf(coffee);
+      if (index > -1) this.coffeeOrder.splice(index, 1);
+  };
+
+  onSubmit() {
+    console.log("Submit");
+  }
+
+}
