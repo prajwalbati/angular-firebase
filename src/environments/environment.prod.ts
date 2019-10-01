d27ec72c689c364d6e70a76f78c7da57d0e662c2
Prajwal Bati <prajwal.bati@ekbana.info>
2019-10-01 15:43:07 +0545

    Sample env added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+declare var require: any;
+const data = require('../../env.json');
+
 export const environment = {
-  production: true
+    production: true,
+    firebaseConfig: data.firebaseConfig
 };
+
