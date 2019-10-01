0fea5e33d18329ecaa8ba3632e68c3e83cc597cf
Prajwal Bati <prajwal.bati@ekbana.info>
2019-10-01 17:57:02 +0545

    Changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,14 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+declare var require: any;
+let data = require('../../env.json');
+
 export const environment = {
-  production: false
+    production: false,
+    firebaseConfig: data.firebaseConfig
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
